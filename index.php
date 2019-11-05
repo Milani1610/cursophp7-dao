b3a3e685e3db18c1e1256b15cd585884e580c6b2
Gabriel Elias Milani <gabriel-elias-milani@hotmail.com>
2019-11-05 16:12:45 -0300

    inserir e alteral

--- index.php
+++ index.php
@@ -17,8 +17,19 @@ require_once("config.php");
 //echo json_encode($search);
 
 //Carrega um usuario usando login ou senha
-$usuario = new Usuario();
-$usuario->login("Gabriel","q1w2e3r4");
+//$usuario = new Usuario();
+//$usuario->login("Gabriel","q1w2e3r4");
+//echo $usuario;
 
+//Inserindo um usuario novo
+//$aluno = new Usuario("Gabriel","@lun1");
+//$aluno->insert();
+//echo $aluno;
+
+//Update de um usuario
+$usuario = new Usuario();
+$usuario->loadByid(8);
+$usuario->update("jose", "456123");
 echo $usuario;
- ?>
+
+?>
