e9aef44f5c2d07a1dd44d68fa74a66748018f0ae
Gabriel Elias Milani <gabriel-elias-milani@hotmail.com>
2019-11-05 15:25:56 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadByid(1);
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadByid(1);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("m");
+//echo json_encode($search);
+
+//Carrega um usuario usando login ou senha
+$usuario = new Usuario();
+$usuario->login("Gabriel","q1w2e3r4");
+
+echo $usuario;
  ?>
